66d0947f9b326fffe6360dff582471d24013c796
chanmob <sellingout@naver.com>
2024-09-06 13:24:54 +0900

    tag 번역 기능 추가

--- lyrics_generator.py
+++ lyrics_generator.py
@@ -1,5 +1,5 @@
 import time
-from openai_client import set_openai_api_key, send_prompt_to_openai
+from openai_client import set_openai_api_key, get_lyrics_from_openai, get_translation_from_openai
 
 # 동요 가사 생성 함수
 def lyrics_composition(keyword):
@@ -15,7 +15,7 @@ def lyrics_composition(keyword):
     """
     
     # OpenAI를 통해 가사 생성
-    lyrics = send_prompt_to_openai(client, prompt)
+    lyrics = get_lyrics_from_openai(client, prompt)
     
     # 가사 정리
     lyrics = lyrics.replace('*', '')
@@ -26,3 +26,26 @@ def lyrics_composition(keyword):
     print(f"Processing time: {end - start:.5f} sec")
     
     return cleaned_lyrics
+
+def tag_translation(tag):
+    """OpenAI API를 통해 가사를 생성"""
+    client = set_openai_api_key()  # OpenAI API 클라이언트 불러오기
+    
+    start = time.time()
+    prompt = f"""
+    음악 장르: {tag}
+
+    이 음악 장르를 영어로 번역해줘.
+    구분 되는 것이 2개 이상이라면 ',' 쉼표로 구분해주고, 다른 말들은 필요 없이 번역된 말만 답변 해줘.
+    """
+    
+    # OpenAI를 태그 번역
+    translated_tag = get_translation_from_openai(client, prompt)
+    
+    # 태그 정리
+    cleaned_tag = translated_tag.replace("'", '')
+    
+    end = time.time()
+    print(f"Processing time: {end - start:.5f} sec")
+    
+    return cleaned_tag
